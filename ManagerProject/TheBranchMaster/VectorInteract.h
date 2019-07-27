befb41c6b0ea42d94df15233db7194c7319712d2
phatdongto <phatdongto@gmail.com>
2019-07-27 23:21:40 +0700

    Export choosen Item to file

--- ManagerProject/TheBranchMaster/VectorInteract.h
+++ ManagerProject/TheBranchMaster/VectorInteract.h
@@ -7,12 +7,13 @@
 
 
 
-class ManageVector 
+class ManageVector
 {
-
 public:
-static bool ImportItemsToVectorA(vector<StorageItem>& VBItems); //VBItems
-	bool ImportOrderToVectorB(vector<BaseOrder>& VBOders); //VOrder
+	static bool ImportItemsToVectorA(vector<StorageItem>& VBItems); //VBItems
+	static bool ImportOrderToVectorB(vector<BaseOrder>& VBOders); //VOrder
 	static bool ExportItemsToTXT(vector<StorageItem>& VBItems);
+	static bool ExportOrdersToTXT(vector<BaseOrder>& VBOders);
 	static string ToString(StorageItem c);
+	static bool ExportChoosenItem(string path, vector<SellItem> Choose);
 };
